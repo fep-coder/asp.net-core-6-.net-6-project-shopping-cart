473fb73c9f8e62eaffcd72cf740ab24c96650eb5
voja <voja@gmail.com>
2022-06-22 12:02:00 +0200

    11 Cart Index

---
+++ ShoppingCart/Controllers/CartController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Infrastructure;
+using ShoppingCart.Models;
+using ShoppingCart.Models.ViewModels;
+
+namespace ShoppingCart.Controllers
+{
+        public class CartController : Controller
+        {
+                private readonly DataContext _context;
+
+                public CartController(DataContext context)
+                {
+                        _context = context;
+                }
+
+                public IActionResult Index()
+                {
+                        List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
+
+                        CartViewModel cartVM = new()
+                        {
+                                CartItems = cart,
+                                GrandTotal = cart.Sum(x => x.Quantity * x.Price)
+                        };
+
+                        return View(cartVM);
+                }
+        }
+}
---
+++ ShoppingCart/Models/ViewModels/CartViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingCart.Models.ViewModels
+{
+        public class CartViewModel
+        {
+                public List<CartItem> CartItems { get; set; }
+                public decimal GrandTotal { get; set; }
+        }
+}
