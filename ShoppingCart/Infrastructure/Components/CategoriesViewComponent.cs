845f7359b22e4a331eb756fb580cc0e0bc9c8905
voja <voja@gmail.com>
2022-06-22 09:55:31 +0200

    06 CategoriesViewComponent

---
+++ ShoppingCart/Infrastructure/Components/CategoriesViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingCart.Infrastructure.Components
+{
+        public class CategoriesViewComponent : ViewComponent
+        {
+                private readonly DataContext _context;
+
+                public CategoriesViewComponent(DataContext context)
+                {
+                        _context = context;
+                }
+
+                public async Task<IViewComponentResult> InvokeAsync() => View(await _context.Categories.ToListAsync());
+        }
+}
