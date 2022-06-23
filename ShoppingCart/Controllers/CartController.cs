21ffb8f80e6eec2d650e362f3d9c832e0b6568ca
voja <voja@gmail.com>
2022-06-23 09:13:55 +0200

    12 Add to cart

--- ShoppingCart/Controllers/CartController.cs
+++ ShoppingCart/Controllers/CartController.cs
@@ -26,5 +26,29 @@ namespace ShoppingCart.Controllers
 
                         return View(cartVM);
                 }
+
+                public async Task<IActionResult> Add(long id)
+                {
+                        Product product = await _context.Products.FindAsync(id);
+
+                        List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
+
+                        CartItem cartItem = cart.Where(c => c.ProductId == id).FirstOrDefault();
+
+                        if (cartItem == null)
+                        {
+                                cart.Add(new CartItem(product));
+                        }
+                        else
+                        {
+                                cartItem.Quantity += 1;
+                        }
+
+                        HttpContext.Session.SetJson("Cart", cart);
+
+                        TempData["Success"] = "The product has been added!";
+
+                        return Redirect(Request.Headers["Referer"].ToString());
+                }
         }
 }
