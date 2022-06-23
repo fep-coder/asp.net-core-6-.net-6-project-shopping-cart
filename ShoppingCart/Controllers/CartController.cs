309cb02fbe142d6e6dd14c4f65b968f749368f10
voja <voja@gmail.com>
2022-06-23 09:33:32 +0200

    13 Decrease Remove and Clear cart

--- ShoppingCart/Controllers/CartController.cs
+++ ShoppingCart/Controllers/CartController.cs
@@ -50,5 +50,61 @@ namespace ShoppingCart.Controllers
 
                         return Redirect(Request.Headers["Referer"].ToString());
                 }
+
+                public async Task<IActionResult> Decrease(long id)
+                {
+                        List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart");
+
+                        CartItem cartItem = cart.Where(c => c.ProductId == id).FirstOrDefault();
+
+                        if (cartItem.Quantity > 1)
+                        {
+                                --cartItem.Quantity;
+                        }
+                        else
+                        {
+                                cart.RemoveAll(p => p.ProductId == id);
+                        }
+
+                        if (cart.Count == 0)
+                        {
+                                HttpContext.Session.Remove("Cart");
+                        }
+                        else
+                        {
+                                HttpContext.Session.SetJson("Cart", cart);
+                        }
+
+                        TempData["Success"] = "The product has been removed!";
+
+                        return RedirectToAction("Index");
+                }
+
+                public async Task<IActionResult> Remove(long id)
+                {
+                        List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart");
+
+                        cart.RemoveAll(p => p.ProductId == id);
+
+                        if (cart.Count == 0)
+                        {
+                                HttpContext.Session.Remove("Cart");
+                        }
+                        else
+                        {
+                                HttpContext.Session.SetJson("Cart", cart);
+                        }
+
+                        TempData["Success"] = "The product has been removed!";
+
+                        return RedirectToAction("Index");
+                }
+
+                public IActionResult Clear()
+                {
+                        HttpContext.Session.Remove("Cart");
+
+                        return RedirectToAction("Index");
+                }
         }
 }
